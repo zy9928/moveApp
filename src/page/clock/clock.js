87b948eca59839206c2610dda5ce24fbbdb7ed35
YuanZheng <992832548@qq.com>
2020-01-15 22:34:56 +0800

    计算根元素rem

--- src/page/clock/clock.js
+++ src/page/clock/clock.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './clock.scss'
 
 const Clock = function Clock () {
   return (
-    <div>计时器</div>
+    <div className="page clockPage" >
+      <div className="pageHeader">
+        <NavLink to="/clock/findWitch" className="findWitchIco">
+          <img src="/asset/find_wtach.png" alt="手表" />
+        </NavLink>
+        <span>计时器</span>
+        <NavLink to="/clock/add" className="addIco">
+          <img src="/asset/add.png" alt="add" />
+        </NavLink>
+        <NavLink to="/active" className="activeIco">
+          <img src="/asset/message.png" alt="通知" />
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
